17e204e1ff0b3fcb994bfcbcaccc27aad3f13b1a
Alex Valladares <alex.valla101@gmail.com>
2020-04-06 22:06:56 -0400

    Checkpoint: Created csv file from extracted data

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "43eb34bdd262215b1c50e8da84924228"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB91b5-1cVq30XCOq8kwAXfoP1OJdPr0lk"
